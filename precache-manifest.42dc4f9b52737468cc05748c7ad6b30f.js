f64ada15710d8692a17e9b7c1da12a8cc542d812
chrisz5109140 <christian123456xy@gmail.com>
2020-04-24 11:56:01 +0800

    Updates

---
+++ precache-manifest.42dc4f9b52737468cc05748c7ad6b30f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dff5f6c1682c555e4985627f43e6ce3",
+    "url": "/react-page-new/index.html"
+  },
+  {
+    "revision": "459900c5ca5072b9743c",
+    "url": "/react-page-new/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "472a6249f39763775042",
+    "url": "/react-page-new/static/js/2.ba9aaf22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-page-new/static/js/2.ba9aaf22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459900c5ca5072b9743c",
+    "url": "/react-page-new/static/js/main.f2aca146.chunk.js"
+  },
+  {
+    "revision": "91cbdcac6d96d48d1c65",
+    "url": "/react-page-new/static/js/runtime-main.356b24f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page-new/static/media/logo.5d5d9eef.svg"
+  }
+]);
